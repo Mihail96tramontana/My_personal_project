9026969fd349b89ea6fa4d48a70aa6c641660846
Mikhail Liubeznow <znakomets2016@yandex.ru>
2026-06-30 09:50:12 +0300

    test: доработаны Allure аннотации и шаги к e2e тестам

--- tests/test_repo_crud_cycle.py
+++ tests/test_repo_crud_cycle.py
@@ -10,7 +10,7 @@ class TestCRUD:
     def test_get_repo(self, base_url, headers, repo_object_fixture):
         with allure.step('Отправляем POST-запрос на создание тестовой сущности - нового репозитория'):
             response = requests.get(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}', headers=headers)
-        with allure.step('Проверяем код ответа, который присылает нам сервер в ответ на запрос'):
+        with allure.step('Проверяем, что отдаваемый сервером код ответа - 200'):
             assert response.status_code == 200, 'Код ответа сервера не 200'
         with allure.step('Проверяем поля, которые отдаёт сервер'):
             assert response.json()['name'] == repo_object_fixture, 'Ошибка в поле "name"'
@@ -21,41 +21,54 @@ class TestCRUD:
     @allure.description('Обновляем описание репозитория методом patch')
     @allure.severity(allure.severity_level.NORMAL)
     def test_patch_description_repo(self, base_url, headers, repo_object_fixture):
-        response = requests.patch(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}',
+        with allure.step('Отправляет PATCH-запрос на обновление поля "description"'):
+            response = requests.patch(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}',
                                   headers=headers,
                                   json={'description':'Обновлённое описание'})
-        assert response.status_code == 200, 'При обновлении описания сервер возвращает код ответа не 200'
-        assert 'Обновлённое описание' in response.json()['description']
+            with allure.step('Проверяем, что отдаваемый сервером код ответа - 200'):
+                assert response.status_code == 200, 'При обновлении описания сервер возвращает код ответа не 200'
+            with allure.step('Проверяем, что значение в поле "description" сменилось'):
+                assert 'Обновлённое описание' in response.json()['description']
 
     @allure.title('Проверяем создание тестовой issue(задачи)')
     @allure.description('Создаём тестовую задачу(issue) методом post')
     @allure.severity(allure.severity_level.NORMAL)
     def test_create_issue(self, base_url, headers, repo_object_fixture):
-        response = requests.post(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}/issues', headers=headers,
+        with allure.step('Отправляем POST-запрос на создание задачи'):
+            response = requests.post(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}/issues', headers=headers,
                                  json={'title': 'Тестовый issue', 'body': 'Описание тестового issue'})
-        assert response.status_code == 201, 'При создании issue код ответа не 200'
-        assert response.json()['title'] == 'Тестовый issue', 'Ошибка в поле "title"'
-        assert response.json()['state'] == 'open', 'Ошибка в поле "state"'
+        with allure.step('Проверяем, что отдаваемый сервером код ответа - 201'):
+            assert response.status_code == 201, 'При создании issue код ответа не 200'
+        with allure.step('Проверяем поля, которые отдаёт сервер'):
+            assert response.json()['title'] == 'Тестовый issue', 'Ошибка в поле "title"'
+            assert response.json()['state'] == 'open', 'Ошибка в поле "state"'
 
     @allure.title('Просмотр созданного issue в списке')
     @allure.description('Просматриваем созданный объект issue в списке методом get')
     @allure.severity(allure.severity_level.NORMAL)
     def test_get_list_issue(self, base_url, headers, repo_object_fixture):
         time.sleep(5)
-        response = requests.get(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}/issues', headers=headers)
-        assert response.status_code == 200, 'Код ответа при просмотре списка issue не 200'
-        assert len(response.json())>0, 'В списке нет объектов'
-        assert 'title' in response.json()[0]
+        with allure.step('Отправляем GET-запрос на просмотр списка задач'):
+            response = requests.get(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}/issues', headers=headers)
+        with allure.step('Проверяем, что отдаваемый сервером код ответа - 200'):
+            assert response.status_code == 200, 'Код ответа при просмотре списка issue не 200'
+        with allure.step('Проверяем, что в списке есть объекты и он не пустой'):
+            assert len(response.json())>0, 'В списке нет объектов'
+        with allure.step('Проверяем, что сервер отдаёт поле "title" в первом объекте'):
+            assert 'title' in response.json()[0]
 
     @allure.title('Закрываем задачу(issue)')
     @allure.description('Закрываем активную задачу(issue) методом patch')
     @allure.severity(allure.severity_level.NORMAL)
     def test_close_issue(self, base_url, headers, repo_object_fixture):
-        response = requests.patch(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}/issues/1',
-                                  headers=headers,
-                                  json={'state': 'closed'})
-        assert response.status_code == 200, 'Неправильный статус код при закрытии задачи(issue)'
-        assert response.json()['state'] == 'closed', 'Ошибка в поле "state"'
+        with allure.step('Отправляем PATCH-запрос на закрытие активной задачи(issue)'):
+            response = requests.patch(f'{base_url}/repos/Mihail96tramontana/{repo_object_fixture}/issues/1',
+                                      headers=headers,
+                                      json={'state': 'closed'})
+        with allure.step('Проверяем, что отдаваемый сервером код ответа - 200'):
+            assert response.status_code == 200, 'Неправильный статус код при закрытии задачи(issue)'
+        with allure.step('Проверяем, что значение в поле "state" = "closed"'):
+            assert response.json()['state'] == 'closed', 'Ошибка в поле "state"'
 
     #после этого последнего теста выполняется teardown из фикстуры repo_object_fixture
 
